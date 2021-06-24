ef2336f0382fd692e27b9638ffc06dd894342658
Delliganesh R <delliganesh_r@anantsol.com>
2021-06-24 22:31:44 +0530

    added dynamic message

--- Main.py
+++ Main.py
@@ -5,7 +5,6 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-
 def Encrypt_key(key):
     password = key.encode()
     salt = b'\x16\x00\xe2\xf6\xd1\xf0c%3\xed\xe0\x87\xba\x11\x17\xed'
@@ -23,9 +22,16 @@ if __name__ == "__main__":
     key = input("Enter Key : ")
     E_key = Encrypt_key(key)
     f = Fernet(E_key)
-    message = "Secrete Meaasge".encode()
+    mes = input("Enter the Message : ")
+    message = mes.encode()
     token = f.encrypt(message)
-    print("Encripted Message : ",token)
-    d_m  = f.decrypt(token).decode()
-    print("Decrepted Message : ",d_m)
+    print("Encrypt Message : ",token)
+    key = input("\nEnter Key to Decrypt : ")
+    D_key = Encrypt_key(key)
+    f = Fernet(D_key)
+    try:
+        d_m  = f.decrypt(token).decode()
+        print("Decrypt Message : ",d_m)
+    except Exception as e:
+        print("Token Invalid",str(e))
     print("Finished")
